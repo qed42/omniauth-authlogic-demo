bcbc780b7365f32ff642b97d715b948a1f9cf79a
Madhusudhan Srinivasa <madhums8@gmail.com>
2011-08-30 09:12:31 +0000

    redirect to edit profile once the auth is destroyed or added to a pre-existing account

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -7,15 +7,17 @@ class AuthorizationsController < ApplicationController
     if current_user
       flash[:notice] = "Successfully added #{omniauth['provider']} authentication"
       current_user.authorizations.create(:provider => omniauth['provider'], :uid => omniauth['uid']) #Add an auth to existing user
+      redirect_to edit_user_path(:current)
     elsif @auth
       flash[:notice] = "Welcome back #{omniauth['provider']} user"
       UserSession.create(@auth.user, true) #User is present. Login the user with his social account
+      redirect_to root_url
     else  
       @new_auth = Authorization.create_from_hash(omniauth, current_user) #Create a new user
       flash[:notice] = "Welcome #{omniauth['provider']} user. Your account has been created."
       UserSession.create(@new_auth.user, true) #Log the authorizing user in.
+      redirect_to root_url
     end
-    redirect_to root_url
   end
   
   def failure
@@ -31,6 +33,6 @@ class AuthorizationsController < ApplicationController
     @authorization = current_user.authorizations.find(params[:id])
     flash[:notice] = "Successfully deleted #{@authorization.provider} authentication."
     @authorization.destroy
-    redirect_to root_url
+    redirect_to edit_user_path(:current)
   end
 end
